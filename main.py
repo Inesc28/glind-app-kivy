8b9e5eac9f18c378385a00ead6d1975ef5c4efe2
Inesc28 <125275762+Inesc28@users.noreply.github.com>
2025-02-22 18:49:06 -0400

    feat: add changes

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
+import requests
 from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.image import Image
 from kivy.uix.boxlayout import BoxLayout
 from views.register import RegisterScreen
+from kivy.core.image import Image as CoreImage
+from io import BytesIO
 
 class MainApp(App):
     def build(self):
         self.title = "Glind"
-        layout = BoxLayout(orientation='vertical')
-        # Agregar las vistas a un BoxLayout (puedes cambiar a un ScreenManager si necesitas cambiar entre pantallas)
-        layout.add_widget(RegisterScreen())
+        layout = FloatLayout()
+
+        url = 'https://img.freepik.com/premium-photo/abstract-black-background-copy-space_1019279-207.jpg'
+        
+        try:
+            response = requests.get(url)
+            response.raise_for_status()  
+            data = BytesIO(response.content)
+            core_image = CoreImage(data, ext="png")
+
+            background_image = Image(texture=core_image.texture, allow_stretch=True, keep_ratio=False)
+            layout.add_widget(background_image)
+        except Exception as e:
+            print(f"Error al descargar o cargar la imagen de fondo: {e}")
+
+        components_layout = BoxLayout(orientation='vertical')
+        components_layout.add_widget(RegisterScreen())
+        
+        layout.add_widget(components_layout)
+
         return layout
 
 if __name__ == "__main__":
--- views/register.py
+++ views/register.py
@@ -1,11 +1,20 @@
+import requests
+from io import BytesIO
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.core.image import Image as CoreImage
 from backend.users import register_new_user
 from views.login_view import LoginScreen
+
+
+Builder.load_file('styles.kv')
+class ButtonLogin(Button):
+    pass
 
 class RegisterScreen(BoxLayout):
 
@@ -14,14 +23,22 @@ class RegisterScreen(BoxLayout):
         self.orientation = 'vertical'
         self.spacing = 20
         self.padding = 20
+        
+        logo_url = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3044f73c-0547-4b01-aeec-7ebff6555e1b/dj1p9o7-af65f7df-e4ef-497f-9cb7-b4545d76045e.png/v1/fill/w_400,h_400/logo_glind_by_coloringdancingedits_dj1p9o7-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDAwIiwicGF0aCI6IlwvZlwvMzA0NGY3M2MtMDU0Ny00YjAxLWFlZWMtN2ViZmY2NTU1ZTFiXC9kajFwOW83LWFmNjVmN2RmLWU0ZWYtNDk3Zi05Y2I3LWI0NTQ1ZDc2MDQ1ZS5wbmciLCJ3aWR0aCI6Ijw9NDAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.e7HioozPY8w7uJYU9ucOlj32A2t67eokOm5vkh3go-A"
+        
+        try:
+            # Descargar la imagen del logo
+            response = requests.get(logo_url)
+            response.raise_for_status()  # Verifica si hubo errores en la descarga
+            data = BytesIO(response.content)
+            core_image = CoreImage(data, ext="png")
 
-        # Crear el logo
-        self.logo = Image(
-            source="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3044f73c-0547-4b01-aeec-7ebff6555e1b/dj1p9o7-af65f7df-e4ef-497f-9cb7-b4545d76045e.png/v1/fill/w_400,h_400/logo_glind_by_coloringdancingedits_dj1p9o7-fullview.png",
-            size_hint=(None, None),
-            size=(120, 160)
-        )
-        self.add_widget(self.logo)
+            # Crear el widget Image con la imagen descargada y ajustarlo
+            self.logo = Image(texture=core_image.texture, size_hint=(None, None), size=(240, 320), pos_hint={'center_x': 0.5, 'center_y': 0.7})
+            self.add_widget(self.logo)
+        except Exception as e:
+            print(f"Error al descargar o cargar el logo: {e}")
+
 
         # Crear el título
         self.title = Label(
@@ -61,19 +78,22 @@ class RegisterScreen(BoxLayout):
             text="Registrar",
             size_hint=(1, None),
             height=40,
-            background_color=(0.6, 0.2, 0.8, 1)
+            background_color=(0.6, 0, 0, 1)
         )
         self.save_button.bind(on_release=self.register_data)
         self.add_widget(self.save_button)
 
-        self.log_button = Button(
+
+        self.log_button = ButtonLogin(
             text="Si ya tienes una cuenta, Inicia Sesión",
-            size_hint=(1, None),
+            size_hint=(None, None),
             height=40,
-            background_color=(0.6, 0.2, 0.8, 1)
+            width=200, 
+            pos_hint={'center_x': 0.5}  
         )
         self.log_button.bind(on_release=self.handle_register)
         self.add_widget(self.log_button)
+        
 
     def register_data(self, instance):
         register_new_user(self.email.text, self.user.text, self.password.text)
