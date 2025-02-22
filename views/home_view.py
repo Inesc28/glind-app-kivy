b78e65c3c26fd18b06917c1a7086504c0ded43ab
Inesc28 <125275762+Inesc28@users.noreply.github.com>
2025-02-22 19:14:02 -0400

    style: new changes styled

--- views/home_view.py
+++ views/home_view.py
@@ -1,8 +1,11 @@
+import requests
+from io import BytesIO
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.image import Image
+from kivy.core.image import Image as CoreImage
 from kivy.uix.actionbar import ActionBar, ActionView, ActionPrevious, ActionButton
 from backend.users import connect_to_server_threaded, get_user_data, get_connected_users
 
@@ -23,7 +26,20 @@ class HomeView(BoxLayout):
             user_code = Label(text="ID desconocido")
             user_name = Label(text="Usuario desconocido")
         
-        logo = Image(source='URL_DEL_LOGO', size_hint=(None, None), size=(100, 120))
+        logo_url = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3044f73c-0547-4b01-aeec-7ebff6555e1b/dj1p9o7-af65f7df-e4ef-497f-9cb7-b4545d76045e.png/v1/fill/w_400,h_400/logo_glind_by_coloringdancingedits_dj1p9o7-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NDAwIiwicGF0aCI6IlwvZlwvMzA0NGY3M2MtMDU0Ny00YjAxLWFlZWMtN2ViZmY2NTU1ZTFiXC9kajFwOW83LWFmNjVmN2RmLWU0ZWYtNDk3Zi05Y2I3LWI0NTQ1ZDc2MDQ1ZS5wbmciLCJ3aWR0aCI6Ijw9NDAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.e7HioozPY8w7uJYU9ucOlj32A2t67eokOm5vkh3go-A"
+        
+        try:
+            # Descargar la imagen del logo
+            response = requests.get(logo_url)
+            response.raise_for_status()  # Verifica si hubo errores en la descarga
+            data = BytesIO(response.content)
+            core_image = CoreImage(data, ext="png")
+
+            # Crear el widget Image con la imagen descargada y ajustarlo
+            self.logo = Image(texture=core_image.texture, size_hint=(None, None), size=(240, 320), pos_hint={'center_x': 0.5, 'center_y': 0.7})
+            self.add_widget(self.logo)
+        except Exception as e:
+            print(f"Error al descargar o cargar el logo: {e}")
         
         # ActionBar como AppBar
         self.appbar = ActionBar(pos_hint={'top': 1})
